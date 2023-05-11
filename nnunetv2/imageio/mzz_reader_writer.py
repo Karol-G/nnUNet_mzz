3e4611001037b351287b2bcbffc27c9f428087f4
Karol Gotkowski <karol.gotkowski@dkfz-heidelberg.de>
2023-05-11 13:48:52 +0200

    Added MZZ imageio

---
+++ nnunetv2/imageio/mzz_reader_writer.py
@@ -0,0 +1,81 @@
+#    Copyright 2021 HIP Applied Computer Vision Lab, Division of Medical Image Computing, German Cancer Research Center
+#    (DKFZ), Heidelberg, Germany
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from typing import Tuple, Union, List
+import numpy as np
+from nnunetv2.imageio.base_reader_writer import BaseReaderWriter
+import mzz
+
+
+class MzzIO(BaseReaderWriter):
+    """
+    Reader and writer for Multi-resolution Zipped Zarr (MZZ) images.
+    """
+
+    supported_file_endings = [
+        '.mzz'
+    ]
+
+    def read_images(self, image_fnames: Union[List[str], Tuple[str, ...]]) -> Tuple[np.ndarray, dict]:
+        print("Read")
+        images = []
+        for f in image_fnames:
+            spacing = (999, 1, 1)
+            img_mzz = mzz.Mzz(path=f)
+            attributes = img_mzz.attrs()
+            img = img_mzz.numpy()
+            if attributes["channel_axis"] is not None:
+                img = np.moveaxis(img, attributes["channel_axis"], 0)
+            if attributes["channel_axis"] is None:
+                img = img[np.newaxis, ...]
+            if attributes["num_spatial"] == 2:
+                img = img[:, np.newaxis, ...]
+            images.append(img)
+
+        if not self._check_all_same([i.shape for i in images]):
+            print('ERROR! Not all input images have the same shape!')
+            print('Shapes:')
+            print([i.shape for i in images])
+            print('Image files:')
+            print(image_fnames)
+            raise RuntimeError()
+
+        return np.vstack(images).astype(np.float32), {'spacing': spacing}
+
+    def read_seg(self, seg_fname: str) -> Tuple[np.ndarray, dict]:
+        seg_mzz = mzz.Mzz(path=seg_fname)
+        attributes = seg_mzz.attrs()
+        seg = seg_mzz.numpy()
+        if attributes["num_spatial"] == 2:
+            seg = seg[np.newaxis, ...]
+        seg = seg[np.newaxis, ...]  # Add 1 channel dim
+        properties = {'spacing': (999, 1, 1)}
+        return seg, properties
+
+    def write_seg(self, seg: np.ndarray, output_fname: str, properties: dict) -> None:
+        print("Write")
+        seg = seg.squeeze()
+        print("Writing MZZ image {}...".format(output_fname))
+        mzz.Mzz(seg).save(output_fname, properties=properties, is_seg=True)
+        print("Finished writing image {}".format(output_fname))
+
+
+if __name__ == '__main__':
+    images = ("/home/k539i/Documents/datasets/original/HMGU_2022_DIADEM/dataset_patches/Dataset107_DIADEMv3/imagesTr/CA0001_insulin_000023_0000.mzz", )
+    segmentation = "/home/k539i/Documents/datasets/original/HMGU_2022_DIADEM/dataset_patches/Dataset107_DIADEMv3/labelsTr/CA0001_insulin_000023.mzz"
+    imgio = MzzIO()
+    img, props = imgio.read_images(images)
+    seg, segprops = imgio.read_seg(segmentation)
+    imgio.write_seg(seg, "/home/k539i/Documents/datasets/original/HMGU_2022_DIADEM/dataset_patches/Dataset107_DIADEMv3/tmp.mzz", {"spacing": (999, 1, 1)})
--- nnunetv2/imageio/reader_writer_registry.py
+++ nnunetv2/imageio/reader_writer_registry.py
@@ -4,6 +4,7 @@ from typing import Type
 from batchgenerators.utilities.file_and_folder_operations import join
 
 import nnunetv2
+from nnunetv2.imageio.mzz_reader_writer import MzzIO
 from nnunetv2.imageio.natural_image_reager_writer import NaturalImage2DIO
 from nnunetv2.imageio.nibabel_reader_writer import NibabelIO, NibabelIOWithReorient
 from nnunetv2.imageio.simpleitk_reader_writer import SimpleITKIO
@@ -16,7 +17,8 @@ LIST_OF_IO_CLASSES = [
     SimpleITKIO,
     Tiff3DIO,
     NibabelIO,
-    NibabelIOWithReorient
+    NibabelIOWithReorient,
+    MzzIO
 ]
 
 
